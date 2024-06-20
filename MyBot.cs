a45046d0d4290dde256806e18ce8f4e31643de24
szymonhel <szhelmecki@gmail.com>
2024-06-20 20:45:33 +0200

    Initial commit

---
+++ MyBot.cs
@@ -0,0 +1,31 @@
+﻿// // <copyright file="MyBot.cs" company="CodePlus Software">
+// // Copyright(c) 2024 All Right Reserved
+// // </copyright>
+// // <author>Szymon Hełmecki</author>
+// // <date>20-06-2024</date>
+// // <summary>MyBot.cs</summary>
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace TestBotBackend;
+
+public class EchoBot : ActivityHandler
+{
+  protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+  {
+    var replyText = $"Echo: {turnContext.Activity.Text}";
+    await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+  }
+
+  protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+  {
+    foreach (var member in membersAdded)
+    {
+      if (member.Id != turnContext.Activity.Recipient.Id)
+      {
+        await turnContext.SendActivityAsync(MessageFactory.Text("Welcome to the bot!"), cancellationToken);
+      }
+    }
+  }
+}
